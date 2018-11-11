10dbec8937bd959823c91b34ea391edf6e769675
Ethan <ethan.r.davidson@gmail.com>
2018-11-11 12:04:42 -0800

    added matmul_1line

--- quantum-factoring/main.js
+++ quantum-factoring/main.js
@@ -16,8 +16,13 @@ const matmul = (v1, v2) => {
 const matmul_es6 = (a, b) => {
   return new Array(a.length).fill(0)
     .map(row => new Array(b[0].length).fill(0))
-    .map((row, i) => row.map((val, j) => a[i].reduce((sum, cur, k) => sum + (cur * b[k][j]), 0)));
+    .map((row, i) => row.map((val, j) => {
+      return a[i].reduce((sum, cur, k) => cur * b[k][j] + sum, 0);
+    }));
 }
+
+const matmul_1line = (a, b) => new Array(a.length).fill(0).map(row => new Array(b[0].length).fill(0)).map((row, i) => row.map((val, j) => a[i].reduce((sum, cur, k) => cur * b[k][j] + sum, 0)));
+
 
 // Tests:
 
@@ -32,3 +37,7 @@ console.log({test_matmul_in, test_matmul_out});
 const test_matmul_es6_in = [[[1, 2, 3],[4, 5, 6]],[[7, 8],[9, 10],[11, 12]]];
 const test_matmul_es6_out = matmul_es6(...test_matmul_es6_in);
 console.log({test_matmul_es6_in, test_matmul_es6_out});
+
+const test_matmul_1line_in = [[[1, 2, 3],[4, 5, 6]],[[7, 8],[9, 10],[11, 12]]];
+const test_matmul_1line_out = matmul_1line(...test_matmul_1line_in);
+console.log({test_matmul_1line_in, test_matmul_1line_out});
