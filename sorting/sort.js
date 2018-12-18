1756a85b513dce5ca6f6bc49205139563ba89666
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2018-12-18 14:39:29 -0800

    added wikipedia links for each sorting algorithm

--- sorting/sort.js
+++ sorting/sort.js
@@ -5,41 +5,41 @@ const insertion = (arr) => {
 };
 
 const selection = (arr) => {
-
+  // https://en.wikipedia.org/wiki/Selection_sort
 };
 
 const merge = (arr) => {
-
+  // https://en.wikipedia.org/wiki/Merge_sort
 };
 
 const heap = (arr) => {
-
+  // https://en.wikipedia.org/wiki/Heapsort
 };
 
 const quick = (arr) => {
-
+  // https://en.wikipedia.org/wiki/Quicksort
 };
 
 const shell = (arr) => {
-
+  // https://en.wikipedia.org/wiki/Shellsort
 };
 
 const bubble = (arr) => {
-
+  // https://en.wikipedia.org/wiki/Bubble_sort
 };
 
 const comb = (arr) => {
-
+  // https://en.wikipedia.org/wiki/Comb_sort
 };
 
 const counting = (arr) => {
-
+  // https://en.wikipedia.org/wiki/Counting_sort
 };
 
 const bucket = (arr) => {
-
+  // https://en.wikipedia.org/wiki/Bucket_sort
 };
 
 const radix = (arr) => {
-
+  // https://en.wikipedia.org/wiki/Radix_sort
 };
